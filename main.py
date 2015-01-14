51dd228424159129164bab2bb989bc0909f2bf50
Comrade Hadi <hadi@pegasusrises.com>
2015-01-14 11:31:52 +0000

    changing deployment method

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('GitHub Hosted Repo GREAT !')
+        self.response.write('GitHub Hosted seems not to be free after all !')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
