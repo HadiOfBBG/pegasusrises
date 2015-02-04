366e462d72686d86381ca1a5416ef292c9b2b644
aliuwahab21 <aliuwahab@yahoo.com>
2015-02-04 21:43:52 +0000

    After working on creating database models using csv specs

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 import webapp2
 from main import MainHandler
-from dbcsv import DbFromCsv,TestClass
+from dbcsv import DbFromCsv
 from dashboard import DashboardHandler
 
 # define all routes here with their appropriate handlers
@@ -8,10 +8,10 @@ from dashboard import DashboardHandler
 # will use in the routes
 routes = [
     (r'/', MainHandler),
-    (r'/testbyaliu', TestClass),
+    (r'/testbyaliu', DbFromCsv),
     (r'/postcsv', DbFromCsv),
      (r'/admin', DashboardHandler),
-     (r'/upload', CSVUploadHandler),
+     # (r'/upload', CSVUploadHandler),
 
 ]
 
@@ -20,7 +20,7 @@ config['webapp2_extras.sessions'] = {
     'secret_key': 'something-very-very-secret',
 }
 
-app = webapp2.WSGIApplication(routes=routes, debug=True, config=config) 
+app = webapp2.WSGIApplication(routes=routes, debug=True, config=config)
 
 # app = webapp2.WSGIApplication([
 # 	('/', MainHandler)
