fb24e5ec25bea137dfadded7553aeb938f711b72
aliuwahab21 <aliuwahab@yahoo.com>
2015-03-02 10:38:40 +0000

    Kaygees request I pushed so he tried the data am returning on pegasus

--- pegasus_db_read.py
+++ pegasus_db_read.py
@@ -20,7 +20,7 @@ class ReadDataFromPegasus(SaveDataIntoPegasusDatabase):
 
 	"""docstring for ReadDataFromPegasus"""
 	def get(self):
-		pass
+		self.queryPegasusDatabase()
 
 
 	def post(self):
@@ -42,31 +42,39 @@ class ReadDataFromPegasus(SaveDataIntoPegasusDatabase):
 		# getting the 'model_properties' property of the returned row
 		model_properties = dynamic_model_and_properties.model_properties
 
+		model_properties_in_json = json.dumps(model_properties)
+		# self.response.out.write(model_properties_in_json)
+		# return
+
 		retrieve_questions_from_pegasus_db = db.Query(Questions)
 		retrieve_questions_from_pegasus_db = list(retrieve_questions_from_pegasus_db)
 		questions_list = self.gql_json_parser(retrieve_questions_from_pegasus_db)
 
 		questions_list = json.dumps(questions_list)
+		# return questions_list
 		# self.response.out.write(questions_list)
+		# return
 
 
 		# retrieve_data_from_pegasus_db = db.Query(BbgDemoModel)
 		retrieve_data_from_pegasus_db = BbgDemoModel.query()
 
 		list_of_data_submissions = self.convert_ndb_expando_queries_into_json(model_properties,retrieve_data_from_pegasus_db)
-		return
+		# return
+		# self.response.out.write(list_of_data_submissions)
+
+		# return
+
 		# json_form_of_retrieved_data_submissions = json.dumps(list_of_data_submissions)
-		# # questions_list = json.dumps(questions_list)
 		# self.response.out.write(json_form_of_retrieved_data_submissions)
+		# return
 
-		data = {'questions_details': questions_list, 'survey_submissions' : retrieve_data_from_pegasus_db}
+		data = {'questions_details': questions_list, 'survey_submissions' : list_of_data_submissions,'model_properties': model_properties_in_json}
 
-		data_returned_to_front_end = json.dumps(data)
+		return data
 
-		self.response.out.write(data)
-
-		return
-
+		# data_returned_to_front_end = json.dumps(data)
+		# self.response.out.write(data)
 
 	# converting db.model queries into list for onward conversion into a json object
 	def gql_json_parser(self,query_obj):
@@ -75,19 +83,29 @@ class ReadDataFromPegasus(SaveDataIntoPegasusDatabase):
 			result.append(dict([(p, unicode(getattr(entry, p))) for p in entry.properties()]))
 		return result
 
+
+
 	def convert_ndb_expando_queries_into_json(self,model_properties,expando_query_objects):
 
-		dictionary_of_query_objects_properties_and_artvalues = {}
+		dictionary_of_query_objects_properties_and_values = {}
 		list_of_query_objects = []
-
-		# for each_query_row in expando_query_objects:
-		# 	self.response.out.write(each_query_row.created_date_time)
-		# return
+		test = "name"
 
 		for each_query_row in expando_query_objects:
-			for each_property in model_properties:
-				dictionary_of_query_objects_properties_and_values[each_property] = each_query_row + "." +each_property
-			list_of_query_objects.append(dictionary_of_query_objects_properties_and_values)
-		return list_of_query_objects
+			for attr, value in each_query_row.__dict__.iteritems():
+				# self.response.out.write(attr)
+				# self.response.out.write(value)
+
+				dictionary_of_query_objects_properties_and_values[attr] = value
+			    # print attr, value
+		return dictionary_of_query_objects_properties_and_values;
+			# list_of_query_objects.append(dictionary_of_query_objects_properties_and_values)
+		# return list_of_query_objects
+
+		# for each_query_row in expando_query_objects:
+		# 	for each_property in model_properties:
+		# 		dictionary_of_query_objects_properties_and_values[each_property] = each_query_row + "." +each_property
+		# 	list_of_query_objects.append(dictionary_of_query_objects_properties_and_values)
+		# return list_of_query_objects
 
 
