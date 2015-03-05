7049d05bce54b83ca62fbdd326bc18c50a5065ad
Comrade Hadi <hadi@pegasusrises.com>
2015-03-05 15:27:34 +0000

    making email module dynamic

--- pegasus_email.py
+++ pegasus_email.py
@@ -1,27 +1,41 @@
 from google.appengine.api import mail
 import webapp2
 from google.appengine.api import users
+import smtplib
 
 
 class EmailHandler():
 	@staticmethod
 	def sendEmail():
+		user_list = ['hadi@pegasusrises.com','francis@pegasusrises.com',
+		'samuel@pegasusrises.com','aliu@pegasusrises.com']
 		user = users.get_current_user()
 
-		message = mail.EmailMessage(sender="comradehadi@gmail.com",
-		                            subject="Pegasus Notification")
+		if user:
+			for each_user in user_list:
+				message = mail.EmailMessage(sender=user.nickname(),
+			                            subject="Pegasus Survey Notification")
 
-		message.to = "Hadi Mukaila <hadi@pegasusrises.com>"
-		message.body = """
-		Hadi Mukaila:
+				message.to = each_user
+				message.body = each_user
+				message.body += """
+				
 
-		Your example.com account has been approved.  You can now visit
-		http://www.example.com/ and sign in using your Google Account to
-		access new features.
+				A survey has been created on the pegasusrises application. You have been
+				invited by to participate in this on ongoing survey. Please follow the following 
+				instructions to respond to the survey.
+				1. Download ODK collect Android app from Google Play.
+				2. Navigate to the platform settings and change the url to pegasusodk.appspot.com
+				3. Press the request forms button to download the questions
+				4. Once you have downloaded the questions, just respond to each question and submit
+				the form afterwards.
 
-		Please let us know if you have any questions.
+				Please let us know if you have any questions.
 
-		The example.com Team
-		"""
+				The Pegasusrises Team
+				"""
 
-		message.send()
+				message.send()
+
+		else:
+			self.response.out.write("User not authenticated")
