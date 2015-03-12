410750d17729c412c3e21b7357181a79e516ae79
aliuwahab21 <aliuwahab@yahoo.com>
2015-03-12 09:22:09 +0000

    After finaly completing and rendering the real survey response returned by aggregate via its URL published method

--- aggregate_db_read.py
+++ aggregate_db_read.py
@@ -38,7 +38,6 @@ class ReadDataFromAggragate(SaveDataIntoPegasusDatabase):
 		if read_raw_data_posted_by_aggregate == None:
 			self.response.out.write('No data currently saved')
 		else:
-			data_list = {}
 			self.response.out.write('The biginining  of the loop over the data\n')
 			for each_raw_data in read_raw_data_posted_by_aggregate:
 				# data_list
@@ -56,7 +55,7 @@ class ReadDataFromAggragate(SaveDataIntoPegasusDatabase):
 
 	def post(self):
 
-		posted_data_by_aggregate = json.dumps(self.request.body)
+		posted_data_by_aggregate = self.request.body
 		self.processPostedByAggreateViaPublish(posted_data_by_aggregate)
 
 
@@ -64,25 +63,20 @@ class ReadDataFromAggragate(SaveDataIntoPegasusDatabase):
 	def processPostedByAggreateViaPublish(self,posted_data_by_aggregate):
 
 		posted_data_by_aggregate = json.loads(posted_data_by_aggregate)
-
 		logging.debug(posted_data_by_aggregate)
 
-		# posted_data_by_aggregate = urllib.unquote(posted_data_by_aggregate)
-		logging.debug("The Form ID: %s", str(posted_data_by_aggregate__class__))
-
-		return
-
 		form_id = posted_data_by_aggregate['formId']
-
-		logging.debug("The Form ID: %s", str(unique_submission_id))
+		logging.debug("The Form ID: %s", str(form_id))
 
 		the_real_inputed_data = posted_data_by_aggregate['data']
-		unique_submission_id = the_real_inputed_data['instanceID']
+		data_object = posted_data_by_aggregate['data'][0]
 
+		unique_submission_id = data_object['instanceID']
 		logging.debug("Unique instance ID is %s", str(unique_submission_id))
 
 		save_posted_data_by_aggregate = SaveAggregateRawPostedData()
 		save_posted_data_by_aggregate.posted_json_data = posted_data_by_aggregate
+		save_posted_data_by_aggregate.survey_responses = data_object
 		save_posted_data_by_aggregate.form_id = form_id
 		save_posted_data_by_aggregate.submission_unique_identity = unique_submission_id
 		save_posted_data_by_aggregate.put()
--- read_data_submissions.py
+++ read_data_submissions.py
@@ -5,6 +5,7 @@ from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 from jinja_template import JinjaTemplating
 from google.appengine.ext import db
+from google.appengine.ext import ndb
 from google.appengine.api import memcache
 from questions_details_from_google_sheets import QuestionsDetailsFromGoogleSheet
 from models.pegasus_model import BbgDemoModel
@@ -17,20 +18,21 @@ from xmltodict import *
 import xmltodict
 from models.questions import Questions
 from pegasus_db_read import ReadDataFromPegasus
+from models.save_raw_aggregate_data import SaveAggregateRawPostedData
 
 
 class ReadDataSubmissions(ReadDataFromPegasus):
 
     def get(self):
-        pass
-        # retrieve_questions_from_pegasus_db = db.Query(Questions)
-        # retrieve_questions_from_pegasus_db = list(retrieve_questions_from_pegasus_db)
-        # questions_list = self.gql_json_parser(retrieve_questions_from_pegasus_db)
-        # data = {'questions_details': questions_list}
-        # data_returned_to_front_end = json.dumps(data)
-        # self.response.out.write(data_returned_to_front_end)
-
-        # return
+        list_of_submissions_objects = []
+        data_submissions = SaveAggregateRawPostedData.query().fetch()
+        for submission in data_submissions:
+            list_of_submissions_objects.append(submission.posted_json_data)
+        # submissions_list = self.gql_json_parser(data_submissions)
+        data = {'submissions': list_of_submissions_objects}
+        data_returned_to_front_end = json.dumps(data)
+        self.response.out.write(data_returned_to_front_end)
+        return
 
 
 
