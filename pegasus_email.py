e8e89a559beb33e05920ee9cbb2d863b7781bff5
Comrade Hadi <hadi@pegasusrises.com>
2015-03-19 13:30:47 +0000

    email responses added

--- pegasus_email.py
+++ pegasus_email.py
@@ -21,7 +21,11 @@ class EmailHandler(JinjaTemplating):
 			self.sendEmail(object_details)
 
 		else:
-			self.response.out.write("Email sending in pegasusrises is done only on a hosted version of Pegasus.")
+			email_result = {
+            "status":"failed",
+            "content":"Email sending in pegasusrises is done only on a hosted version of Pegasus."
+            }
+			return self.response.out.write(json.dumps(email_result))
 		
 
 
@@ -36,34 +40,53 @@ class EmailHandler(JinjaTemplating):
 		user = users.get_current_user()
 
 		if user:
+
+			try:
 			
-			message = mail.EmailMessage(sender=user.email(),
-									subject="Pegasus Survey Notification")
+				message = mail.EmailMessage(sender=user.email(),
+										subject="Pegasus Survey Notification")
 
-			message.to = email_list
-			
-			message.body = """
-			
+				message.to = email_list
+				
+				message.body = """
+				
 
-			A survey has been created on the pegasusrises application. You have been
-			invited by to participate in this ongoing survey. Please follow the following 
-			instructions to respond to the survey.
-			1. Download ODK collect Android app from Google Play if you have not.
-			2. Navigate to the platform settings and change the url to pegasusodk.appspot.com if you have not
-			3. Press the request forms button to download the questions
-			4. Once you have downloaded the questions, just respond to each question and submit
-			the form afterwards.
+				A survey has been created on the pegasusrises application. You have been
+				invited by to participate in this ongoing survey. Please follow the following 
+				instructions to respond to the survey.
+				1. Download ODK collect Android app from Google Play if you have not.
+				2. Navigate to the platform settings and change the url to pegasusodk.appspot.com if you have not
+				3. Press the request forms button to download the questions
+				4. Once you have downloaded the questions, just respond to each question and submit
+				the form afterwards.
 
-			Please let us know if you have any questions.
+				Please let us know if you have any questions.
 
-			The Pegasusrises Team
-			"""
+				The Pegasusrises Team
+				"""
 
-			message.send()
+				message.send()
+				email_result = {
+            "status":"failed",
+            "content":"success"
+            }
+				return self.response.out.write(json.dumps(email_result))
+
+			except Exception,e:
+				email_result = {
+            "status":"failed",
+            "content":"failed"
+            }
+				return self.response.out.write(json.dumps(email_result))
 
 		else:
+			email_result = {
+            "status":"failed",
+            "content":"User not authenticated"
+            }
+			return self.response.out.write(json.dumps(email_result))
 
-			logging.error("User not authenticated")
+			# logging.error("User not authenticated")
 
 	
 
